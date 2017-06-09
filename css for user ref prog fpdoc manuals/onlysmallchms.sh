d2e474b104a811a14bc4b0bd938f1e5141c55335
zbyna <zbynek.fiala@gmail.com>
2017-06-10 00:18:21 +0200

    Added dark style css

---
+++ css for user ref prog fpdoc manuals/onlysmallchms.sh
@@ -0,0 +1,4 @@
+./compilelatexchm prog "Programmer's Guide"
+./compilelatexchm user "User's Guide"
+./compilelatexchm ref "Reference Guide"  ref/ref.kwd
+./compilelatexchm fpdoc "FPDoc documentation"
